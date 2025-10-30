79fcaff0502b3a725360127d463a4843800cb9b1
Fajar Anugrah <fajaranugrah@Fajars-MacBook-Air.local>
2025-10-30 14:48:35 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d041d912bd9a288a854f95cd98b7094ce2fb998f28ee72efb92fe5b220ee5cb3
+size 525
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1248f512bcf74b93d5d2bee45a7a3765eb25b824566429b0595fa2dbd31d7d33
+size 13091
